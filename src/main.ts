d91ed31269c6f48038ce8bfe31f9afab592daf1f
Andrew Bentley <andrew.bentley@altana.ai>
2026-02-14 10:15:43 -0500

    feat: add game scene with collision, scoring, lives, waves, game over

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import * as ex from 'excalibur';
 import { CONFIG } from './config';
-import { Player } from './actors/player';
-import { AlienGrid } from './actors/alien-grid';
+import { GameScene } from './scenes/game';
 
 const game = new ex.Engine({
   width: CONFIG.canvas.width,
@@ -9,13 +8,6 @@ const game = new ex.Engine({
   backgroundColor: ex.Color.fromHex('#0a0a2a'),
 });
 
-const player = new Player();
-game.add(player);
-
-const alienGrid = new AlienGrid(game.currentScene);
-
-game.on('preupdate', (evt: ex.PreUpdateEvent) => {
-  alienGrid.update(evt.delta, CONFIG.alien.fireInterval);
-});
-
+game.add('game', new GameScene());
+game.goToScene('game');
 game.start();
