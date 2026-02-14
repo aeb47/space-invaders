a3e749add47f2319281a6fa788be61672d83d7ca
Andrew Bentley <andrew.bentley@altana.ai>
2026-02-14 10:14:41 -0500

    feat: add alien grid with formation movement and shooting

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as ex from 'excalibur';
 import { CONFIG } from './config';
 import { Player } from './actors/player';
+import { AlienGrid } from './actors/alien-grid';
 
 const game = new ex.Engine({
   width: CONFIG.canvas.width,
@@ -10,5 +11,11 @@ const game = new ex.Engine({
 
 const player = new Player();
 game.add(player);
+
+const alienGrid = new AlienGrid(game.currentScene);
+
+game.on('preupdate', (evt: ex.PreUpdateEvent) => {
+  alienGrid.update(evt.delta, CONFIG.alien.fireInterval);
+});
 
 game.start();
